a04833b9fa06d94b9f8b718b367b9ddb39cb489e
Vlad Gheorghiu <vsoftco@gmail.com>
2021-01-17 16:11:02 -0500

    refactoring

--- README.md
+++ README.md
@@ -1,33 +1,43 @@
-### Python console program for renumbering latex references
+# pytexnumber.py
 
- - Usage: 
-    
-    Python 3 version:
+Renumbers LaTeX equations. The program reads from the standard input and writes to the standard output.
 
-        python3 py3texnumber.py in.tex out.tex pattern replacement 
-    
-    Python 2 version:
-    
-        python py2texnumber.py in.tex out.tex pattern replacement 
+### usage:
 
-- Description: replaces all labels in `in.tex` that match 
-a given input pattern, such as `\label{pattern*}` 
-and all corresponding references (`\ref`, `\eqref`, `\pageref`) 
-with renumbered ones `replacement+idx`, where `idx` counts 
-the matching `\label{pattern*}` order of appearance, 
-i.e. equals 1 for the first `\label` that matches the input pattern, 
-2 for the second etc. The `in.tex` is left unchanged and the 
-modifications are written to `out.tex`.
+    pytexnumber.py [-h] [--ignore-comments] [--no-ignore-comments]
+                   [--log LOG] [pattern] [replacement]
 
-- Example: consider a latex file `source.tex` having three labels, named, 
-in order of appearance, `\label{eqn5}`, `\label{eqn_important}` 
-and lastly `\label{entropy}`. Then the command 
+### positional arguments:
 
-        python3 py3texnumber.py in.tex out.tex eqn Eqn 
+    pattern               input pattern, e.g., "eqn"
+    replacement           replacement pattern, e.g., "Eqn"
 
-will replace `\label{eqn5}` by `\label{Eqn1}` (and also all references 
-to `eqn5` will automatically become references to `Eqn1`) and replace 
-`\label{eqn_important}` by `\label{Eqn2}` (again together with 
-the corresponding references). The `\label{entropy}` remains unchanged 
-since it does not match the input pattern `\label{eqn*}`. 
-The changes are saved to the file `out.tex`.
+### optional arguments:
+
+    -h, --help            show this help message and exit
+    --ignore-comments     ignore comments, true by default
+    --no-ignore-comments  do not ignore comments, false by default
+    --log LOG             log file
+
+### Description
+
+Replaces all labels from the input stream (in this case redirected from `in.tex`) that match a given input pattern, such
+as `\label{pattern*}`
+and all corresponding references (`\ref`, `\eqref`, `\pageref`) with renumbered ones `replacement+idx`, where `idx`
+counts the matching `\label{pattern*}` order of appearance, i.e., equals 1 for the first `\label` that matches the input
+pattern, 2 for the second etc. The output is written to the standard output (in this case redirected to `out.tex`).
+Optionally, writes the replacement pattern sequence to `log_file`. Warnings and errors are output to the standard error
+stream.
+
+### Example
+
+Consider a LaTeX file `in.tex` having three labels, named, in order of appearance, `\label{eqn5}`
+, `\label{eqn_important}`and lastly `\label{entropy}`. Then the command
+
+    python3 pytexnumber.py <in.tex >out.tex eqn Eqn 
+
+will replace `\label{eqn5}` by `\label{Eqn1}` (and also all references to `eqn5`
+will automatically become references to `Eqn1`) and replace
+`\label{eqn_important}` by `\label{Eqn2}` (again together with the corresponding references). The `\label{entropy}`
+remains unchanged since it does not match the input pattern `\label{eqn*}`. The changes are saved to the file `out.tex`,
+and the replacement pattern sequence is written to the file `log.txt`.
---
+++ pytexnumber.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python3
+
+# pytexnumber.py
+#
+# Renumbers LaTeX references
+#
+# Type python3 pytexnumber.py --help for help
+
+# Copyright (c) 2013 - 2021 Vlad Gheorghiu. All rights reserved.
+#
+# MIT License
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import argparse
+import re
+import sys
+
+
+def build_labels(input_file, pattern_in, ignore_comments):
+    count = 0  # number of distinct labels in the dictionary
+    dictionary = {}  # this is the label dictionary
+    warnings = []  # duplicate labels warning
+    for line_no, cur_line in enumerate(input_file, start=1):
+        if ignore_comments is True:
+            cur_line = cur_line.split('%')[0]
+        # search for \label{pattern...} in the current line
+        for labels in re.finditer('\\\\label{'
+                                  + pattern_in + '.*?}', cur_line):
+            # extract {pattern...} from \label{pattern...}
+            label = re.search('{.*?}', labels.group()).group()
+            # insert the UNIQUE label {pattern...} into the label dictionary
+            if label not in dictionary:
+                count += 1
+                dictionary[label] = count
+            else:
+                col_no = labels.start() + 1  # the warning's column number
+                warnings.append(['\\label' + label, line_no, col_no])
+    input_file.seek(0)  # go back to the beginning of the file
+    return [dictionary, warnings]
+
+
+def replace_refs_in_line(keywords, pattern_in, pattern_out,
+                         dictionary, line, line_idx, ignore_comments):
+    warnings = []  # undefined reference(s) warning(s) in (the current line)
+    line_no_comments = line
+    comment = ""
+    if ignore_comments is True:
+        line_split = line.split('%', 1)
+        line_no_comments = line_split[0]
+        if len(line_split) > 1:  # we have a comment
+            comment = '%' + line_split[1]
+
+    for keyword in keywords:
+        for matches in re.finditer('\\\\' + keyword +
+                                   '{' + pattern_in + '.*?}', line_no_comments):
+            # extract {pattern...} from \<keyword>{pattern...}
+            match = re.search('{.*?}', matches.group()).group()
+            # do the replacement whenever the {pattern...} is different
+            # from the dictionary key
+            if match not in dictionary:
+                # undefined reference
+                col_no = matches.start() + 1  # the warning's column number
+                warnings.append(['\\' + keyword + match, line_idx, col_no])
+            if (match in dictionary and
+                    ('{' + pattern_out + str(dictionary[match]) + '}') != match):
+                line_no_comments = re.sub(keyword + match,
+                                          keyword + '_REPLACED_mark'
+                                          + '{' + pattern_out +
+                                          str(dictionary[match]) + '}', line_no_comments)
+        line_no_comments = re.sub(keyword + '_REPLACED_mark', keyword, line_no_comments)
+    return [line_no_comments + comment, warnings]
+
+
+# Main program
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Renumbers LaTeX equations.'
+                    'The program reads from the standard input and writes to the standard output.')
+    parser.add_argument('pattern', nargs='?', help='input pattern, e.g., "eqn"')
+    parser.add_argument('replacement', nargs='?', help='replacement pattern, e.g., "Eqn"')
+    parser.add_argument('--ignore-comments', dest='comments', action='store_true',
+                        help='ignore comments, true by default')
+    parser.add_argument('--no-ignore-comments', dest='comments', action='store_false',
+                        help='do not ignore comments, false by default')
+    parser.set_defaults(comments=True)
+    parser.add_argument('--log', help='log file')
+
+    args = parser.parse_args()
+
+    pattern = args.pattern  # pattern to replace
+    replacement = args.replacement  # replacement
+    ignore_comments = args.comments  # ignore LaTeX comments
+
+    keywords = ['label', 'eqref', 'ref', 'pageref']  # modify as needed
+
+    try:
+        with sys.stdin as f_in, sys.stdout as f_out:
+            # create the label dictionary
+            [label_dictionary, label_warnings] = build_labels(f_in, pattern, ignore_comments)
+            # search and replace
+            modified_lines = []  # list with the lines that are modified
+            distinct_label_modifications = 0  # count modified \label{pattern...}
+            reference_warnings = []  # reference warnings
+            # line by line...
+            for line_index, current_line in enumerate(f_in, start=1):
+                [modified_line, warnings] = \
+                    replace_refs_in_line(keywords, pattern, replacement, label_dictionary,
+                                         current_line, line_index, ignore_comments)
+                if modified_line != current_line:  # the line was modified
+                    modified_lines += [line_index]
+                if warnings:  # append reference warning(s) from the current line
+                    reference_warnings += warnings
+                f_out.write(modified_line)
+
+            if args.log is not None:
+                try:
+                    with open(args.log, 'w') as logfile:
+                        original_stdout = sys.stdout  # Save a reference to the original standard output
+                        sys.stdout = logfile
+
+                        # replacements
+                        for item in sorted(label_dictionary, key=label_dictionary.get):
+                            item_no_accolades = item
+                            remove = ['{', '}']
+                            for c in remove:
+                                item_no_accolades = item_no_accolades.replace(c, '')
+                            print(item_no_accolades + ' -> ' + replacement + str(label_dictionary[item]))
+
+                        # display warnings
+                        sys.stdout = sys.stderr
+                        if label_warnings or reference_warnings:
+                            if reference_warnings:
+                                print('PARSING WARNING: Undefined references')
+                                for [item, row, col] in reference_warnings:
+                                    print(item + ', ' + str(row) + ':' + str(col))
+                            if label_warnings:
+                                print('PARSING WARNING: Duplicate labels')
+                                for [item, row, col] in label_warnings:
+                                    print(item + ', ' + str(row) + ':' + str(col))
+
+                        sys.stdout = original_stdout
+                except IOError as err:
+                    print(str(err))
+    except IOError as err:
+        print(str(err))
